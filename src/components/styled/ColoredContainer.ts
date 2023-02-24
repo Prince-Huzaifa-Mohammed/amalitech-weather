2507e48c188813f854483a87a3d67d55e3bd8e2f
genesis <prince.mohammed@amalitech.org>
2023-02-24 12:09:04 +0000

    Implemented a reusable container component.

--- src/components/styled/ColoredContainer.ts
+++ src/components/styled/ColoredContainer.ts
@@ -2,10 +2,42 @@ import styled from "styled-components";
 
 interface Props {
   height: string;
-  color: string;
+  backgroundColor: string;
+}
+
+interface ContainerProps {
+  width?: string;
 }
 
 export const ColoredContainer = styled.div<Props>`
   height: ${({ height }) => height};
-  background-color: ${({ color }) => color};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
+
+export const Container = styled.div<ContainerProps>`
+  width: ${({ width }) => width || "1000px"};
+  max-width: 100%;
+  padding: 0 2rem;
+  margin: 0 auto;
+`;
+
+export const PositionedBox = styled.div<ContainerProps>`
+  width: ${({ width }) => width || "1000px"};
+  max-width: 100%;
+  padding: 2rem 3rem;
+  background-color: ${({ theme }) => theme.colors.white};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: ${({ theme }) => theme.borderRadius.slightlyRound};
+  /* display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem; */
+
+  @media (max-width: 1110px) {
+    width: 90%;
+  }
 `;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
-import { ColoredContainer } from "../components/styled/ColoredContainer";
+import {
+  ColoredContainer,
+  PositionedBox,
+} from "../components/styled/ColoredContainer";
 
 const Home = () => {
   return (
     <>
-      <ColoredContainer height="50vh" color="#DD5928">
+      <ColoredContainer height="50vh" backgroundColor="#DD5928">
         Ghana
       </ColoredContainer>
 
-      <ColoredContainer height="50vh" color="#0198BA">
+      <ColoredContainer height="50vh" backgroundColor="#0198BA">
         Ghana
       </ColoredContainer>
+
+      <PositionedBox>Senegal</PositionedBox>
     </>
   );
 };
